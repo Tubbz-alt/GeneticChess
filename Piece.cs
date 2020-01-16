50668b629e9c260e4f8f7c2917c135adc7d6e3bf
UnknownDrone097 <48077045+UnknownDrone097@users.noreply.github.com>
2020-01-16 14:15:46 -0500

    Update Piece.cs

--- Piece.cs
+++ Piece.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Net;
 
 namespace GeneticChess
 {
@@ -10,39 +12,91 @@ namespace GeneticChess
     {
         double Value;
         string Abreviation;
-        public string PictureURL;
+        public bool IsWhite;
+        public Image PieceImage; 
+        string PictureURL = "https://cdn5.vectorstock.com/i/1000x1000/15/29/chess-pieces-including-king-queen-rook-pawn-knight-vector-2621529.jpg";
         public virtual Piece[,] Move(Piece[,] board) { return null; }
+        protected Image GetImage(int x1, int y1, int x2, int y2)
+        {
+            var request = WebRequest.Create(PictureURL);
+            using (var response = request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            {
+                Bitmap b = Bitmap.FromStream(stream) as Bitmap;
+                Rectangle rectangle = new Rectangle(x1, y1, x2, y2);
+                System.Drawing.Imaging.PixelFormat format = b.PixelFormat;
+                Bitmap b2 = b.Clone(rectangle, format);
+                //b2.MakeTransparent(Color.White);
+                b2 = new Bitmap(b2, new Size(40, 40));
+                for (int i = 0; i < b2.Height; i++)
+                {
+                    for (int ii = 0; ii < b2.Width; ii++)
+                    {
+                        Color pixelColor = b2.GetPixel(i, ii);
+                        if (pixelColor.GetBrightness() > .5) { b2.SetPixel(i, ii, Color.Transparent); }
+                        else {  b2.SetPixel(i, ii, Color.HotPink); }
+                    }
+                }
+                return b2 as Image;
+            };
+        }
     }
     class Pawn : Piece
     {
-        public Pawn()
+        public Pawn(bool iswhite)
         {
-            PictureURL = "https://media.istockphoto.com/vectors/chess-pawn-icon-element-of-minimalistic-icon-for-mobile-concept-and-vector-id948733576?k=6&m=948733576&s=612x612&w=0&h=HFHf_VyUghVVBDxJvokkjxiqZZ9RC3bmdH1Rb63VT0k=";
+            IsWhite = iswhite;
+            if (iswhite) { PieceImage = GetImage(85, 100, 200, 200); }
+            else { PieceImage = GetImage(85, 385, 200, 200); }
         }
         public override Piece[,] Move(Piece[,] board) { return null; }
     }
     class Knight : Piece
     {
-
+        public Knight(bool iswhite)
+        {
+            IsWhite = iswhite;
+            if (iswhite) { PieceImage = GetImage(700, 60, 230, 230); }
+            else { PieceImage = GetImage(700, 350, 230, 230); }
+        }
     }
     class Bishop : Piece
     {
-
+        public Bishop(bool iswhite)
+        {
+            IsWhite = iswhite;
+            if (iswhite) { PieceImage = GetImage(500, 75, 200, 200); }
+            else { PieceImage = GetImage(500, 150, 200, 200); }
+        }
     }
     class Rook : Piece
     {
-
+        public Rook(bool iswhite)
+        {
+            IsWhite = iswhite;
+            if (iswhite) { PieceImage = GetImage(295, 90, 200, 200); }
+            else { PieceImage = GetImage(295, 370, 200, 200); }
+        }
     }
     class Queen : Piece
     {
-
+        public Queen(bool iswhite)
+        {
+            IsWhite = iswhite;
+            if (iswhite) { PieceImage = GetImage(85, 400, 200, 200); }
+            else { PieceImage = GetImage(165, 400, 200, 200); }
+        }
     }
     class King : Piece
     {
-
+        public King(bool iswhite)
+        {
+            IsWhite = iswhite;
+            if (iswhite) { PieceImage = GetImage(165, 400, 200, 200); }
+            else { PieceImage = GetImage(385, 400, 200, 200); }
+        }
     }
     class Empty : Piece
     {
-
     }
 }
